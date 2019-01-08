85d0be0d0f91cd5405528cf8a05c53515364c2ba
sergiomc95 <43533774+sergiomc95@users.noreply.github.com>
2019-01-08 18:20:19 +0100

    Update uaserver.py
    servidor funcionando

--- uaserver.py
+++ uaserver.py
@@ -6,9 +6,13 @@ import sys
 import time
 import socketserver
 from xml.sax import make_parser
+from proxy_registrar import log_writer
 from xml.sax.handler import ContentHandler
 
 usage_error = 'usage: python3 uaserver.py <fichero>'
+
+sdp_body = 'Content-Type: application/sdp\r\n\r\nv=0\r\n'
+sdp_body += 'o=username serverip\r\ns=nombresesion\r\nt=0\r\nm=audio puertortp RTP\r\n'
 
 class XMLHandler(ContentHandler):
 
@@ -35,10 +39,52 @@ class XMLHandler(ContentHandler):
 
 class SIPHandler(socketserver.DatagramRequestHandler):
 
+    comando = []
+
     def handle(self):
 
-        info = self.read().decode('utf-8')
-        print(info)
+        info = self.rfile.read().decode('utf-8')
+        direccion_cliente = self.client_address[0] + ':' + str(self.client_address[1])
+        log_mess = 'Received from ' + direccion_cliente + ': '
+        log_mess += info.replace('\r\n', ' ')
+        log_writer(log_mess, config)
+        print('Recibido:\n' + info)
+        metodo = info.split()[0]
+        if metodo == 'INVITE':
+            self.comando.append(info.split('\r\n')[4].split()[-1])
+            sesion = info.split('\r\n')[5].split('=')[1]
+            self.comando.append(info.split('\r\n')[7].split()[1])
+            mess = 'SIP/2.0 100 Trying\r\n\r\n'
+            mess += 'SIP/2.0 180 Ringing\r\n\r\n'
+            mess += 'SIP/2.0 200 OK\r\n'
+            sdp = sdp_body.replace('username', config['account_username'])
+            sdp = sdp.replace('serverip', config['uaserver_ip'])
+            sdp = sdp.replace('nombresesion', sesion)
+            sdp = sdp.replace('puertortp', config['rtpaudio_puerto'])
+            mensaje = mess + sdp
+            self.wfile.write(bytes(mensaje, 'utf-8'))
+            print('Enviado:\n' + mensaje)
+            log_mess = 'Sent to ' + direccion_cliente + ': '
+            log_mess += mensaje.replace('\r\n', ' ')
+            log_writer(log_mess, config)
+        elif metodo == 'ACK':
+            comando = './mp32rtp -i ' + self.comando[0] + ' -p ' + self.comando[1] + ' < '
+            comando += config['audio_path']
+            print('Ejecutando:', comando)
+            os.system(comando)
+            self.comando = []
+        elif metodo == 'BYE':
+            self.wfile.write(b'SIP/2.0 200 OK\r\n')
+            print('Enviado:\nSIP/2.0 200 OK')
+            log_mess = 'Sent to ' + direccion_cliente + ': '
+            log_mess += 'SIP/2.0 200 OK'
+            log_writer(log_mess, config)
+        else:
+            self.wfile.write(b'SIP/2.0 405 Method Not Allowed\r\n')
+            print('Enviado:\nSIP/2.0 405 Method Not Allowed')
+            log_mess = 'Sent to ' + direccion_cliente + ': '
+            log_mess += 'SIP/2.0 405 Method Not Allowed'
+            log_writer(log_mess, config)
 
 if __name__ =='__main__':
 
@@ -59,7 +105,9 @@ if __name__ =='__main__':
 
     serv = socketserver.UDPServer((ip, port), SIPHandler)
     try:
+        log_writer('Starting...', config)
         print('El servidor de', name, 'esta ahora activo')
         serv.serve_forever()
     except KeyboardInterrupt:
+        log_writer('Finishing.', config)
         print('El servidor de', name, 'ha acabado su trabajo por hoy')
